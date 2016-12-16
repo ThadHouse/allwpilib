c15c9a2e12cfab2af0c0b824fd46b31af1421d98
Thad House <thadhouse1@gmail.com>
2016-12-16 13:12:50 -0800

    Updates a few DS things

--- halUnitTests/include/DSWrapper.h
+++ halUnitTests/include/DSWrapper.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-void StartDsLoop();
+bool StartDsLoop();
 
 void SetJoystickHookedUp(int joystick, bool isHookedUp);
 
--- halUnitTests/src/DSWrapper.cpp
+++ halUnitTests/src/DSWrapper.cpp
@@ -1,8 +1,17 @@
 #include "DSWrapper.h"
 
 #include "llvm/SmallString.h"
-
+#include "llvm/raw_ostream.h"
 #include "HAL/cpp/priority_mutex.h"
+#include "HAL/Notifier.h"
+#include "HAL/HAL.h"
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+
+#include <thread>
 
 priority_mutex joystickMutex;
 
@@ -21,8 +30,63 @@ struct Joystick {
 
 static Joystick joysticks[6];
 
-void StartDsLoop() {
+static HAL_NotifierHandle sendThreadHandle;
+static std::thread receiveThread;
 
+#define DS_RECEIVE_PORT 1150
+#define DS_SEND_PORT 1110
+
+static int fd = -1;
+
+static void dsSendLoop(uint64_t currentTime, HAL_NotifierHandle handle) {
+  if (fd <= 0) goto done;
+
+  struct sockaddr_in addr;
+  memset((char *)&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_LOOPBACK;
+  addr.sin_port = htons(DS_SEND_PORT);
+
+
+  char buf[10];
+  sendto(fd, buf, 10, 0, (struct sockaddr*) &addr, sizeof(addr));
+
+done:
+  int32_t status = 0;
+  HAL_UpdateNotifierAlarm(handle, currentTime + 20000ULL, &status); // 20 milliseconds
+}
+
+bool StartDsLoop() {
+  if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    //llvm::errs << "Could not create socket";
+    return false;
+  }
+
+  struct sockaddr_in addr;
+  memset((char *)&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(0);
+
+  if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    //llvm::errs << "bind failed";
+    close(fd);
+    fd = -1;
+    return false;
+  }
+
+  int32_t status = 0;
+  sendThreadHandle = HAL_InitializeNotifierThreaded(dsSendLoop, nullptr, &status);
+  if (status != 0) {
+    close(fd);
+    fd = -1;
+    return false;
+  }
+  uint64_t curTime = HAL_GetFPGATime(&status);
+  if (status == 0)
+    HAL_UpdateNotifierAlarm(sendThreadHandle, curTime + 20000ULL,
+                            &status);
+  return true;
 }
 
 void SetJoystickHookedUp(int joystick, bool isHookedUp) {
