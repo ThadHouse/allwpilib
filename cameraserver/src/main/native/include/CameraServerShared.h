93d712bbf37bc8665c6b93a3dd17b2f530e3da4b
Thad House <thadhouse1@gmail.com>
2018-04-27 21:52:52 -0700

    New 2018 and later build setup

---
+++ cameraserver/src/main/native/include/CameraServerShared.h
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <utility>
+
+#include <llvm/StringRef.h>
+
+namespace frc {
+
+typedef void (*ReportUsbCameraFunc)(int);
+typedef void (*ReportAxisCameraFunc)(int);
+typedef void (*ReportVideoServerFunc)(int);
+typedef void (*SetCameraServerErrorFunc)(llvm::StringRef);
+typedef void (*SetVisionRunnerErrorFunc)(llvm::StringRef);
+typedef void (*ReportDriverStationErrorFunc)(llvm::StringRef);
+typedef std::pair<std::thread::id, bool> (*GetRobotMainThreadIdFunc)();
+
+class CameraServerShared {
+ public:
+  virtual void ReportUsbCamera(int id) = 0;
+  virtual void ReportAxisCamera(int id) = 0;
+  virtual void ReportVideoServer(int id) = 0;
+  virtual void SetCameraServerError(llvm::StringRef error) = 0;
+  virtual void SetVisionRunnerError(llvm::StringRef error) = 0;
+  virtual void ReportDriverStationError(llvm::StringRef error) = 0;
+  virtual std::pair<std::thread::id, bool> GetRobotMainThreadId() const = 0;
+};
+
+void SetCameraServerShared(std::unique_ptr<CameraServerShared> shared);
+CameraServerShared* GetCameraServerShared();
+}  // namespace frc
