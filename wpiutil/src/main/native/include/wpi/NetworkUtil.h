8f9e0e27a41474d415ee1d5566a0823b1fc35a8d
Thad House <thadhouse1@gmail.com>
2018-10-28 21:57:46 -0700

    work on addr

---
+++ wpiutil/src/main/native/include/wpi/NetworkUtil.h
@@ -0,0 +1,11 @@
+#ifndef WPIUTIL_WPI_NETWORKUTIL_H_
+#define WPIUTIL_WPI_NETWORKUTIL_H_
+
+#include <string>
+#include <vector>
+
+namespace wpi {
+std::vector<std::string> GetNetworkInterfaces();
+}
+
+#endif  // WPIUTIL_WPI_NETWORKUTIL_H_
