4ad2fd3a8796e984415e30407a1934e2c5fc4338
Thad House <thadhouse1@gmail.com>
2022-01-27 22:39:48 -0800

    Lots of work on window

--- riolog/src/main/native/cpp/App.cpp
+++ riolog/src/main/native/cpp/App.cpp
@@ -56,38 +56,19 @@ struct TeamNumberRefHolder {
 };
 
 static std::unique_ptr<TeamNumberRefHolder> teamNumberRef;
-static std::unordered_map<std::string, std::pair<unsigned int, std::string>>
-    foundDevices;
 static wpi::Logger logger;
 static sysid::DeploySession deploySession{logger};
 static std::unique_ptr<wpi::MulticastServiceResolver> multicastResolver;
 static glass::MainMenuBar gMainMenu;
+static glass::LogData gLogData;
+static glass::LogView gLogView{&gLogData};
+static bool autoScroll = true;
 
-static void FindDevices() {
-  WPI_EventHandle resolveEvent = multicastResolver->GetEventHandle();
-
-  bool timedOut = 0;
-  if (wpi::WaitForObject(resolveEvent, 0, &timedOut)) {
-    auto allData = multicastResolver->GetData();
-
-    for (auto&& data : allData) {
-      // search for MAC
-      auto macKey =
-          std::find_if(data.txt.begin(), data.txt.end(),
-                       [](const auto& a) { return a.first == "MAC"; });
-      if (macKey != data.txt.end()) {
-        auto& mac = macKey->second;
-        foundDevices[mac] = std::make_pair(data.ipv4Address, data.hostName);
-      }
-    }
-  }
-}
 
 static int minWidth = 400;
 
 static void DisplayGui() {
   int& teamNumber = teamNumberRef->teamNumber;
-  FindDevices();
 
   ImGui::GetStyle().WindowRounding = 0;
 
@@ -107,6 +88,17 @@ static void DisplayGui() {
   gMainMenu.WorkspaceMenu();
   gui::EmitViewMenu();
 
+  bool config = false;
+  if (ImGui::BeginMenu("Control")) {
+    if (ImGui::MenuItem("Clear")) {
+      gLogData.Clear();
+    }
+    if (ImGui::MenuItem("Configure")) {
+      config = true;
+    }
+    ImGui::EndMenu();
+  }
+
   bool about = false;
   if (ImGui::BeginMenu("Info")) {
     if (ImGui::MenuItem("About")) {
@@ -116,16 +108,24 @@ static void DisplayGui() {
   }
   ImGui::EndMenuBar();
 
+  if (config) {
+    ImGui::OpenPopup("Config");
+  }
+  if (ImGui::BeginPopupModal("Config")) {
+    ImGui::Checkbox("Auto Scroll?", &autoScroll);
+    if (ImGui::Button("Close")) {
+      ImGui::CloseCurrentPopup();
+    }
+    ImGui::EndPopup();
+  }
+
   if (about) {
     ImGui::OpenPopup("About");
   }
   if (ImGui::BeginPopupModal("About")) {
-    ImGui::Text("roboRIO Team Number Setter");
+    ImGui::Text("riolog");
     ImGui::Separator();
     ImGui::Text("v%s", GetWPILibVersion());
-    ImGui::Separator();
-    ImGui::Text("Has mDNS Implementation: %d",
-                static_cast<int>(multicastResolver->HasImplementation()));
     ImGui::Separator();
     ImGui::Text("Save location: %s", glass::GetStorageDir().c_str());
     if (ImGui::Button("Close")) {
@@ -134,108 +134,16 @@ static void DisplayGui() {
     ImGui::EndPopup();
   }
 
-  if (multicastResolver->HasImplementation()) {
-    ImGui::InputInt("Team Number", &teamNumber);
-
-    if (teamNumber < 0) {
-      teamNumber = 0;
+  if (ImGui::BeginPopupContextWindow()) {
+    if (ImGui::MenuItem("Clear")) {
+      gLogData.Clear();
     }
-
-    int nameWidth = ImGui::CalcTextSize("roboRIO2-0000-FRC.local. ").x;
-    int macWidth = ImGui::CalcTextSize("88:88:88:88:88:88").x;
-    int ipAddressWidth = ImGui::CalcTextSize("255.255.255.255").x;
-    int setWidth = ImGui::CalcTextSize(" Set Team To 99999 ").x;
-    int blinkWidth = ImGui::CalcTextSize(" Blink ").x;
-    int rebootWidth = ImGui::CalcTextSize(" Reboot ").x;
-
-    minWidth = nameWidth + macWidth + ipAddressWidth + setWidth + blinkWidth +
-               rebootWidth + 100;
-
-    std::string setString = fmt::format("Set team to {}", teamNumber);
-
-    if (ImGui::BeginTable("Table", 6)) {
-      ImGui::TableSetupColumn(
-          "Name",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          nameWidth);
-      ImGui::TableSetupColumn(
-          "MAC Address",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          macWidth);
-      ImGui::TableSetupColumn(
-          "IP Address",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          ipAddressWidth);
-      ImGui::TableSetupColumn(
-          "Set",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          setWidth);
-      ImGui::TableSetupColumn(
-          "Blink",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          blinkWidth);
-      ImGui::TableSetupColumn(
-          "Reboot",
-          ImGuiTableColumnFlags_NoResize | ImGuiTableColumnFlags_WidthFixed,
-          rebootWidth);
-      ImGui::TableHeadersRow();
-
-      for (auto&& i : foundDevices) {
-        ImGui::TableNextRow();
-        ImGui::TableNextColumn();
-        ImGui::Text("%s", i.second.second.c_str());
-        ImGui::TableNextColumn();
-        ImGui::Text("%s", i.first.c_str());
-        ImGui::TableNextColumn();
-        struct in_addr in;
-        in.s_addr = i.second.first;
-        ImGui::Text("%s", inet_ntoa(in));
-        ImGui::TableNextColumn();
-        std::future<int>* future = deploySession.GetFuture(i.first);
-        ImGui::PushID(i.first.c_str());
-        if (future) {
-          ImGui::Button("Deploying");
-          ImGui::TableNextColumn();
-          ImGui::TableNextColumn();
-          const auto fs = future->wait_for(std::chrono::seconds(0));
-          if (fs == std::future_status::ready) {
-            deploySession.DestroyFuture(i.first);
-          }
-        } else {
-          if (ImGui::Button(setString.c_str())) {
-            deploySession.ChangeTeamNumber(i.first, teamNumber, i.second.first);
-          }
-          ImGui::TableNextColumn();
-          if (ImGui::Button("Blink")) {
-            deploySession.Blink(i.first, i.second.first);
-          }
-          ImGui::TableNextColumn();
-          if (ImGui::Button("Reboot")) {
-            deploySession.Reboot(i.first, i.second.first);
-          }
-        }
-        ImGui::PopID();
-      }
-
-      ImGui::EndTable();
-    }
-
-    ImGui::Columns(6, "Devices");
-
-    // TODO make columns better
-  } else {
-    // Missing MDNS Implementation
-    ImGui::Text("mDNS Implementation is missing.");
-#ifdef _WIN32
-    ImGui::Text("Windows 10 1809 or newer is required for this tool");
-#else
-    ImGui::Text("avahi-client 3 and avahi-core 3 are required for this tool");
-    ImGui::Text(
-        "Install libavahi-client3 and libavahi-core3 from your package "
-        "manager");
-#endif
+    ImGui::Checkbox("Auto Scroll?", &autoScroll);
+    ImGui::EndPopup();
   }
-  ImGui::Columns();
+
+  glass::DisplayLog(&gLogData, autoScroll);
+
   ImGui::End();
 
   glfwSetWindowSizeLimits(gui::GetSystemWindow(), minWidth, 200, GLFW_DONT_CARE,
@@ -259,7 +167,7 @@ void Application(std::string_view saveDir) {
   gui::AddIcon(riolog::GetResource_riolog_256_png());
   gui::AddIcon(riolog::GetResource_riolog_512_png());
 
-  glass::SetStorageName("roborioteamnumbersetter");
+  glass::SetStorageName("riolog");
   glass::SetStorageDir(saveDir.empty() ? gui::GetPlatformSaveFileDir()
                                        : saveDir);
 
